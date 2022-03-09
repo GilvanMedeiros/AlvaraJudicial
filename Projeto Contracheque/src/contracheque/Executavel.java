e40a4d0f27efc03fc2d0921576729469d46031b0
Gilvan <Gilvan@Gilvan-PC.lan>
2022-03-09 18:06:48 -0300

    Atualização

--- Projeto Contracheque/src/contracheque/Executavel.java
+++ Projeto Contracheque/src/contracheque/Executavel.java
@@ -8,25 +8,39 @@ public class Executavel {
 		
 		BaseCalculo baseCalculo = new BaseCalculo();
 		String salario = JOptionPane.showInputDialog("Digite o salario");
-		String produtividade = JOptionPane.showInputDialog("Digite a produtividade");
-		String admissao = JOptionPane.showInputDialog("Digite a datta de admissão");
+		String produtividade = JOptionPane.showInputDialog("Digite a produtividade");		
 				
 		baseCalculo.setSalario(Double.parseDouble(salario));
-		baseCalculo.setProdutividade(Double.parseDouble(produtividade));	
-		baseCalculo.setDataAdmissão(admissao);
+		baseCalculo.setProdutividade(Double.parseDouble(produtividade));		
 		
-		//Imprimindo
-		System.out.println("Data de Admissão: " + baseCalculo.getDataAdmissão());
+		int confirmacao = JOptionPane.showConfirmDialog(null, "Os dados foram inseridos corretamente ?");		
 		
-		System.out.println("Salário: R$ " + baseCalculo.getSalario());
+		if (confirmacao == 0) {
+			
+			JOptionPane.showMessageDialog(null, "Salario -> R$ " + salario);
+			JOptionPane.showMessageDialog(null, "Produtividade -> R$ " + produtividade);
+			JOptionPane.showMessageDialog(null, "Valor Bruto -> R$ " + baseCalculo.getValorBruto());
+			JOptionPane.showMessageDialog(null, "30% do INSS -> R$ " + baseCalculo.getPorcentagem());			
+			
+			double impostoDeRenda = (baseCalculo.getValorBruto() * 30) / 100;
+			double liquido = baseCalculo.getLiquido() - impostoDeRenda;
+			
+			if (baseCalculo.getValorBruto() > 2500.00) {
+				JOptionPane.showMessageDialog(null, "Valor Ultrapassou o teto que é de -> R$ 2500.00");
+				JOptionPane.showMessageDialog(null, "Desconto de 40% do impostor de renda -> R$ " + impostoDeRenda);
+				JOptionPane.showMessageDialog(null, "Valor Líquido com IRR -> R$ " + liquido);				
+			} else {
+				JOptionPane.showMessageDialog(null, "Valor Líquido -> R$ " + baseCalculo.getLiquido() + " Não ultrapassou o teto que é de R$ -> 2.500");
+			}			
+			
+		}
 		
-		System.out.println("Produtividade: R$ " + baseCalculo.getProdutividade());
 		
-		System.out.println("Bruto: R$ " + baseCalculo.getValorBruto());
 		
-		System.out.println("30% INSS : " + baseCalculo.getPorcentagem());
 		
-		System.out.println("Líquido: " + baseCalculo.getLiquido());			
+		
+		
+		
 	
 	}
 
